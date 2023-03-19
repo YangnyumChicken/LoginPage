886696c525a9d213548a4c62385c666810a8a2b6
lsjkeh9683 <lsjkeh9683@naver.com>
2023-03-19 16:57:13 +0900

    connect login.js

--- app/app.js
+++ app/app.js
@@ -2,14 +2,16 @@
 //모듈
 const express = require('express');
 const app = express();
-//앱 세팅
-app.set("views", "./src/views");
-app.set("view engine", "ejs");
+
 //라우팅
 const home = require('./src/routes/home');
 
-app.use('/', home);
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
+app.use('/', home);
 const PORT = 3000;
 
 module.exports = app;
