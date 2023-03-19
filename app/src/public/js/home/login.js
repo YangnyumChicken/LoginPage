2668d5201d49431aa54272ad77423ad141ce3913
lsjkeh9683 <lsjkeh9683@naver.com>
2023-03-19 17:57:15 +0900

    login backend

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,7 +11,15 @@ function login(){
         id : id.value,
         pw : pw.value,
     };
-    console.log(req);
+    
+    fetch("/login", {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+        }, 
+        body : JSON.stringify(req),
+    }).then((res)=> res.json())
+      .then(console.log)
 }
 
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,43 @@
 "use strict";
 
-const hello  = (req, res)=>{
-    res.render("home/index");
+const users = {
+    id: ["root", "phantom"],
+    pw: ["2017", "2017"]
+};
+
+const output = {
+    home : (req, res)=>{
+        res.render("home/index");
+    },
+    login : (req,res)=>{
+        res.render("home/login");
+    },
+
 }
 
-function login(req,res){
-    res.render("home/login");
+const process = {
+    login : (req, res)=>{
+        const id = req.body.id;
+        const pw = req.body.pw;
+
+       if(users.id.includes(id)){
+        const idx = users.id.indexOf(id);
+
+        if(users.pw[idx] === pw){
+            return res.json({
+                success : true,
+            })
+        }
+       }
+
+       return res.json({
+        success : false,
+        msg : "login false"
+       })
+    }
 }
 
 module.exports = {
-    hello,
-    login
+    output,
+    process
 }
